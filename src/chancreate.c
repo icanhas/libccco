30a76dc796dd97ca06b3ce61725e63a317e59e19
ich <ich@sunn>
2013-02-11 08:19:57 -0500

    fix chancreate

--- src/chancreate.c
+++ src/chancreate.c
@@ -1,13 +1,15 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include "csp.h"
 #include "dat.h"
 #include "fn.h"
 
 Chan*
-chancreate(int elsz, int nel)
+chancreate(int nel, int elsz)
 {
 	Chan *c;
 	Lock *l;
+	Cond *full, *flushed;
 	
 	c = malloc(sizeof *c);
 	if(c == nil){
@@ -17,11 +19,31 @@ chancreate(int elsz, int nel)
 	l = createlock();
 	if(l == nil){
 		errorf("chancreate -- failed to alloc lock\n");
+		free(c);
+		return nil;
+	}
+	full = createcond();
+	if(full == nil){
+		errorf("chancreate -- createcond failed\n");
+		free(c);
+		free(l);
+		return nil;
+	}
+	flushed = createcond();
+	if(flushed == nil){
+		errorf("chancreate -- createcond failed\n");
+		free(c);
+		free(l);
+		free(full);
 		return nil;
 	}
 	c->l = l;
+	c->full = full;
+	c->flushed = flushed;
+	c->isfull = 0;
 	c->elsz = elsz;
-	c->nel = nel;
-	c->b = nil;
+	c->max = nel;
+	c->nel = 0;
+	c->b = malloc(nel * elsz);
 	return c;
 }
