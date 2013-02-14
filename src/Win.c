65ae41d271cf6c1be132e2ceec48b2bf46b745ba
ich <icanhas@users.sf.net>
2013-02-14 02:25:25 +0000

    windows: get this up to speed again

--- src/Win.c
+++ src/Win.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <windows.h>
@@ -9,11 +10,21 @@ struct _Lock {
 	CRITICAL_SECTION	l;
 };
 
+struct _Cond {
+	HANDLE c;
+};
+
 struct _Thread {
+	char*	name;
 	void	(*fn)(void*);	/* proc 'main' */
 	void*	arg;		/* proc args */
 	HANDLE	h;		/* thread handle */
+	Cond	wake;
+	int	running;
+	Rand	r;
 };
+
+static DWORD	tlsindex = -1;
 
 static DWORD WINAPI	run(void*);
 
@@ -21,14 +32,20 @@ Thread*
 createthread(void (*fn)(void*), void *arg, int stksz)
 {
 	Thread *t;
+	Cond wake;
 	LPTHREAD_START_ROUTINE entr;
 	HANDLE h;
 	
 	t = malloc(sizeof *t);
 	if(t == nil)
 		return nil;
+	if(tlsindex == -1)
+		tlsindex = TlsAlloc();
+	t->name = nil;
 	t->fn = fn;
 	t->arg = arg;
+	t->wake = wake;
+	t->running = 0;
 	entr = (LPTHREAD_START_ROUTINE)&run;
 	h = CreateThread(nil, stksz, entr, t, 0, nil);
 	if(h == nil){
@@ -37,6 +54,8 @@ createthread(void (*fn)(void*), void *arg, int stksz)
 		return nil;
 	}
 	t->h = h;
+	t->running = 1;
+	signal(&t->wake);
 	return t;
 }
 
@@ -47,6 +66,35 @@ freethread(Thread *t)
 		return;
 	CloseHandle(t->h);
 	free(t);
+}
+
+void
+exitthread(void)
+{
+	ExitThread(0);
+}
+
+void
+setthreadname(const char *name)
+{
+	Thread *t;
+	
+	t = (Thread*)TlsGetValue(tlsindex);
+	assert(t != nil);
+	if(t->name != nil)
+		free(t->name);
+	t->name = malloc(strlen(name)+1);
+	strcpy(t->name, name);
+}
+
+char*
+getthreadname(void)
+{
+	Thread *t;
+	
+	t = (Thread*)TlsGetValue(tlsindex);
+	assert(t != nil);
+	return t->name;
 }
 
 Lock*
@@ -63,25 +111,139 @@ createlock(void)
 	return l;
 }
 
-int
-destroylock(Lock *l)
+void
+freelock(Lock *l)
 {
-	DeleteCriticalSection(&l->l);
-	return 0;
+	assert(l != nil);
+	destroylock(l);
+	free(l);
 }
 
 int
-lock(Lock *l)
+initlock(Lock *l)
 {
-	EnterCriticalSection(&l->l);
+	assert(l != nil);
+	InitializeCriticalSection(&l->l);
+	return 0;
+}
+
+void
+destroylock(Lock *l)
+{
+	assert(l != nil);
+	DeleteCriticalSection(&l->l);
+}
+
+int
+lock(Lock *l, int blocking)
+{
+	assert(l != nil);
+	if(blocking){
+		EnterCriticalSection(&l->l);
+		return 1;
+	}
+	if(TryEnterCriticalSection(&l->l))
+		return 1;
 	return 0;
 }
 
 int
 unlock(Lock *l)
 {
+	assert(l != nil);
 	LeaveCriticalSection(&l->l);
 	return 0;
+}
+
+Cond*
+createcond(void)
+{
+	Cond *p;
+	
+	p = malloc(sizeof *p);
+	if(p == nil){
+		errorf("createcond -- out of memory\n");
+		return nil;
+	}
+	if(initcond(p) != 0){
+		free(p);
+		return nil;
+	}
+	return p;
+}
+
+void
+freecond(Cond *c)
+{
+	assert(c != nil);
+	destroycond(c);
+	free(c);
+}
+
+int
+initcond(Cond *c)
+{
+	assert(c != nil);
+	c->c = CreateEvent(NULL, FALSE, FALSE, TEXT("Cond"));
+	if(c->c == nil){
+		errorf("initcond -- CreateEvent failed\n");
+		return -1;
+	}
+	return 0;
+}
+
+void
+destroycond(Cond *c)
+{
+	assert(c != nil);
+	CloseHandle(c->c);
+}
+
+int
+wait(Cond *c, Lock *l)
+{
+	assert(c != nil);
+	assert(l != nil);
+	unlock(l);
+	if(WaitForSingleObject(c->c, INFINITE) != WAIT_OBJECT_0)
+		return -1;
+	lock(l, 1);
+	return 0;
+}
+
+int
+signal(Cond *c)
+{
+	assert(c != nil);
+	if(!SetEvent(c->c))
+		return -1;
+	return 0;
+}
+
+void
+_procsleep(long dur)
+{
+	Sleep(dur * 1000);
+}
+
+void
+_stlrand(ulong seed)
+{
+	Thread *t;
+	
+	t = (Thread*)TlsGetValue(tlsindex);
+	assert(t != nil);
+	_srand(&t->r, seed);
+}
+
+ulong
+_tlrand(void)
+{
+	Thread *t;
+	
+	t = (Thread*)TlsGetValue(tlsindex);
+	assert(t != nil);
+	return _rand(&t->r);
 }
 
 /*
@@ -91,8 +253,22 @@ static DWORD WINAPI
 run(void *arg)
 {
 	Thread *t;
+	Lock fake;
 	
 	t = (Thread*)arg;
+	TlsSetValue(tlsindex, t);
+	
+	initrand(&t->r);
+	
+	initlock(&fake);
+	lock(&fake, 1);
+	while(!t->running){
+		dprintf("thread suspend\n");
+		wait(&t->wake, &fake);
+	}
+	unlock(&fake);
+	destroylock(&fake);
+	dprintf("thread %p starts\n", t);
 	t->fn(t->arg);
 	return nil;
 }
